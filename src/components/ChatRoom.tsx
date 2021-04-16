caaddd9cc23f0abdd3eb45d287076a2b94439e07
michaelcbarilla <michael@eeproto.com>
2021-04-15 23:22:39 -0400

    start chat component

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 
-const ChatRoom = () => (
+import '../styles/ChatRoom.css';
 
-    <h1>The Chat Room Component</h1>
-  
-);
+const ChatRoom = () => {
+  console.log()
+  return (
+    <Card>
+      <Card.Header>
+        <h3 className="display-5 text-center">
+          Chat with Mike123 and Jerry123
+        </h3>
+      </Card.Header>
+      <Card.Body>
+        <div className="d-flex justify-content-start mb-4">
+          <div className="msg_cotainer">
+            Hi, how are you Mike?
+            <span className="msg_time">8:40 AM, Today</span>
+          </div>
+        </div>
+        <div className="d-flex justify-content-end mb-4">
+          <div className="msg_cotainer_send">
+            Hi Jerry i am good thanks. how about you?
+            <span className="msg_time_send">8:55 AM, Today</span>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
 
 export default ChatRoom;
---
+++ src/models/thread.model.tsx
@@ -0,0 +1,18 @@
+import { Friend } from './friend.model';
+
+export class Thread {
+  constructor(
+    public id: string,
+    public createdBy: string,
+    public created: string,
+    public name: string,
+    public participants: Friend[],
+    public lastMessage: {
+      id: string,
+      seq: number,
+      timestamp: number,
+      from: string,
+      content: string
+    }
+  ) {}
+}
