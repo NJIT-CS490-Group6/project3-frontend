4d4d436b263526b0508b894be5e424d64243f3ae
michaelcbarilla <michael@eeproto.com>
2021-04-15 22:59:07 -0400

    add max height to friends list/ change to cleaner scroll bar

--- src/components/FriendsList.tsx
+++ src/components/FriendsList.tsx
@@ -51,6 +51,141 @@ const allFriends: Friend[] = [
     timestamp: "2017-07-21T17:32:28Z"
     }
   ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645ce92a",
+    "jerry123",
+    "Jerry Smith",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645gf44a",
+    "mike123",
+    "Mike Low",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa64511111",
+    "Alex65",
+    "Alex Derry",
+    {
+    status: "busy",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645fd135",
+    "Darian123",
+    "Darian Puccio",
+    {
+    status: "offline",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645bvcd3",
+    "Tyler999",
+    "Tyler Smith",
+    {
+    status: "offline",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645ce92a",
+    "jerry123",
+    "Jerry Smith",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645gf44a",
+    "mike123",
+    "Mike Low",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa64511111",
+    "Alex65",
+    "Alex Derry",
+    {
+    status: "busy",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645fd135",
+    "Darian123",
+    "Darian Puccio",
+    {
+    status: "offline",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645bvcd3",
+    "Tyler999",
+    "Tyler Smith",
+    {
+    status: "offline",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645ce92a",
+    "jerry123",
+    "Jerry Smith",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645gf44a",
+    "mike123",
+    "Mike Low",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa64511111",
+    "Alex65",
+    "Alex Derry",
+    {
+    status: "busy",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645fd135",
+    "Darian123",
+    "Darian Puccio",
+    {
+    status: "offline",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645bvcd3",
+    "Tyler999",
+    "Tyler Smith",
+    {
+    status: "offline",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
 ];
 
 const FriendsList = () => {
@@ -106,7 +241,7 @@ const FriendsList = () => {
         <span><div id="busy-square" />Busy</span>
         <span><div id="offline-square" />Offline</span>
       </div>
-      {!isLoading && <ListGroup defaultActiveKey="#link1">
+      {!isLoading && <ListGroup defaultActiveKey="#link1" className="friends-list">
         {friends.map(friend => (
           <ListGroup.Item 
             action 
