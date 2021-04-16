4d3cc876feca8e4eb80f022c95728c6e8c210e16
michaelcbarilla <michael@eeproto.com>
2021-04-16 19:29:09 -0400

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -14,11 +14,11 @@ test('app rendering/navigating', () => {
   )
   // verify page content for expected route
   // often you'd use a data-testid or role query, but this is also possible
-  expect(screen.getByText(/The Landing Page/i)).toBeInTheDocument()
+  expect(screen.getByText(/By Group 6/i)).toBeInTheDocument()
 
   const leftClick = { button: 0 }
   userEvent.click(screen.getByText(/Dashboard/i), leftClick)
 
   // check that the content changed to the new page
-  expect(screen.getByText(/The Chat Room Component/i)).toBeInTheDocument()
+  expect(screen.getByText(/Available/i)).toBeInTheDocument()
 });
