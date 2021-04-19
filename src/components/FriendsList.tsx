b8d81926221b2db54f17d3312396d4486e900eff
michaelcbarilla <michael@eeproto.com>
2021-04-19 16:06:54 -0400

    remove unused test data/fix eslint

--- src/components/FriendsList.tsx
+++ src/components/FriendsList.tsx
@@ -5,60 +5,10 @@ import FriendsToolbar from './FriendsToolbar';
 
 import '../styles/FriendsList.css';
 
-// const allFriends: Friend[] = [
-//   new Friend(
-//     "f48c1eca-295d-4603-8433-bbfa645ce92a",
-//     "jerry123",
-//     "Jerry Smith",
-//     {
-//     status: "available",
-//     timestamp: "2017-07-21T17:32:28Z"
-//     }
-//   ),
-//   new Friend(
-//     "f48c1eca-295d-4603-8433-bbfa645gf44a",
-//     "mike123",
-//     "Mike Low",
-//     {
-//     status: "available",
-//     timestamp: "2017-07-21T17:32:28Z"
-//     }
-//   ),
-//   new Friend(
-//     "f48c1eca-295d-4603-8433-bbfa64511111",
-//     "Alex65",
-//     "Alex Derry",
-//     {
-//     status: "busy",
-//     timestamp: "2017-07-21T17:32:28Z"
-//     }
-//   ),
-//   new Friend(
-//     "f48c1eca-295d-4603-8433-bbfa645fd135",
-//     "Darian123",
-//     "Darian Puccio",
-//     {
-//     status: "offline",
-//     timestamp: "2017-07-21T17:32:28Z"
-//     }
-//   ),
-//   new Friend(
-//     "f48c1eca-295d-4603-8433-bbfa645bvcd3",
-//     "Tyler999",
-//     "Tyler Smith",
-//     {
-//     status: "offline",
-//     timestamp: "2017-07-21T17:32:28Z"
-//     }
-//   )
-// ];
-
 const FriendsList = () => {
   const hasFetchedData = useRef(false);
   const [friends, setFriends] = useState<Friend[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
-
 
   const onClickFriend = () => {
     alert("Pull up friend profile");
@@ -73,6 +23,7 @@ const FriendsList = () => {
         body: JSON.stringify({username})
     })
     const data = await response.json();
+    console.log(data);
   }
 
   const getVariant: any = (status: string) => {
