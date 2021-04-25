c19ca9d2cafff21c18a2f3b8e3f856456ebe876a
John Salerno <jjs77@njit.edu>
2021-04-25 16:37:22 -0400

    added in dummy data for thread and friends for testing. also started work on logic to get data from backend. made changes to bootstrap styling of chatroom list. displayed chatroomlist on main dashboard

--- src/components/ChatRoomList.tsx
+++ src/components/ChatRoomList.tsx
@@ -1,6 +1,94 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import React, { useState, useEffect, useRef } from "react";
 import { Thread } from '../models/thread.model';
+import { Friend } from '../models/friend.model';
+
+import "../styles/ChatRoomList.css";
+
+const thread1Friends: Friend[] = [
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645ce92a",
+    "jerry123",
+    "Jerry Smith",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645gf44a",
+    "mike123",
+    "Mike Low",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  )
+];
+
+const thread2Friends: Friend[] = [
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa64511111",
+    "Alex65",
+    "Alex Derry",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645fd135",
+    "Darian123",
+    "Darian Puccio",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  ),
+  new Friend(
+    "f48c1eca-295d-4603-8433-bbfa645bvcd3",
+    "Tyler999",
+    "Tyler Smith",
+    {
+    status: "available",
+    timestamp: "2017-07-21T17:32:28Z"
+    }
+  )
+];
+
+const allThreads: Thread[] = [
+  new Thread(
+    "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "f48c1eca-295d-4603-8433-bbfa645ce92a",
+    "2021-04-25T19:09:35.752Z",
+    "Jerry Smith",
+    thread1Friends,
+    {
+      id: "h62c1eca-295d-4603-8433-bbfa645ca28b",
+      seq: 0,
+      timestamp: "2021-04-25T19:10:26.093Z",
+      from: "f48c1eca-295d-4603-8433-bbfa645ce92a",
+      content: "testing message"
+    }
+  ),
+  new Thread(
+    "3fa85f64-5717-4562-b3fc-2c963f66cde3",
+    "f48c1eca-295d-4603-8433-bbfa64511111",
+    "2021-04-25T20:32:58.415Z",
+    "Alex Derry",
+    thread2Friends,
+    {
+      id: "h62c1eca-295d-4603-8433-bbfa645fd45",
+      seq: 0,
+      timestamp: "2021-04-25T20:33:46.415Z",
+      from: "f48c1eca-295d-4603-8433-bbfa64511111",
+      content: "another test"
+    }
+  )
+];
+
+
+
 
 const ChatRoomList = () => {
   const hasFetchedData = useRef(false);
@@ -12,6 +100,22 @@ const ChatRoomList = () => {
   };
 
   useEffect(() => {
+    const fetchThreadsHandler = () => {
+      setIsLoading(true);
+      setTimeout(() => {
+        setIsLoading(false);
+        setThreads(allThreads);
+      }, 1000);
+    }
+    if (!hasFetchedData.current) {
+      fetchThreadsHandler();
+      hasFetchedData.current = true;
+    }
+  }, [isLoading]);
+
+
+/*
+  useEffect(() => {
     const myAbortController = new AbortController();
     const fetchThreadsHandler = async () => {
       setIsLoading(true);
@@ -21,16 +125,18 @@ const ChatRoomList = () => {
       const data = await response.json();
 
       const allThreads: Thread[] = [];
-      // for (let i = 0; i < data.length; i += 1) {
-      //   const status = mapStatus(data[i].status);
-      //   const friend = new Friend(
-      //     data[i].id,
-      //     data[i].username,
-      //     `${data[i].first_name} ${data[i].last_name}`,
-      //     { status, timestamp: data[i].created_at }
-      //   );
-      //   allFriends.push(friend);
-      // }
+      for (let i = 0; i < data.length; i += 1) {
+        const thread = new Thread(
+          //need work in here ****************************
+          data[i].id,
+          data[i].createdBy,
+          data[i].created,
+          data[i].name,
+          data[i].participants,
+          data[i].lastMessage
+        );
+        allThreads.push(thread);
+      }
       setThreads(allThreads);
       setIsLoading(false);
     };
@@ -44,18 +150,24 @@ const ChatRoomList = () => {
       myAbortController.abort();
     };
   }, []);
+*/
 
   return (
     <div>
       {!isLoading && (
         <ListGroup defaultActiveKey="#link1" className="threads-list">
+          <div className="heading">Open chatrooms:</div>
           {threads.map((thread) => (
             <ListGroup.Item
               action
               onClick={onClickThread}
               key={thread.id}
+              variant="info"
             >
-              {thread.participants[0].username}
+              Chatroom with:&nbsp;
+              {thread.participants.map((user) => (
+                  `${user.username}, `
+              ))}
             </ListGroup.Item>
           ))}
         </ListGroup>
