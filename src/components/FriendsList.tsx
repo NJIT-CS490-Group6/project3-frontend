acc182aaccfaff97da17fc38184d550cd7417b88
michaelcbarilla <michael@eeproto.com>
2021-04-27 15:37:12 -0400

    stash changes

--- src/components/FriendsList.tsx
+++ src/components/FriendsList.tsx
@@ -26,7 +26,7 @@ const FriendsList = () => {
       {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({ username }),
       }
